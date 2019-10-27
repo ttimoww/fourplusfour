de1bd70b0762dd92720c7e64ce0230872162a471
Timo <ttimoww@gmail.com>
2019-10-27 16:21:22 +0100

    Added thumbnail to archive page

--- woocommerce.php
+++ woocommerce.php
@@ -1,3 +1,11 @@
 <?php get_header() ?>
-    <?php woocommerce_content(); ?>
+    <?php 
+        if ( is_singular( 'product' ) ) {
+            woocommerce_content();
+           }else{
+           //For ANY product archive.
+           //Product taxonomy, product search or /shop landing
+            woocommerce_get_template( 'archive-product.php' );
+           }
+    ?>
 <?php get_footer() ?>
